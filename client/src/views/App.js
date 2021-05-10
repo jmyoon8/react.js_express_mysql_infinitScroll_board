2902320c23b4b3e9e339f66161095e9f35fa548c
jmyoon8 <61464819+jmyoon8@users.noreply.github.com>
2021-05-10 13:34:50 +0900

    Update App.js

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,6 +1,6 @@
 import React,{ Suspense} from 'react';
 import {BrowserRouter as Router, Switch, Route} from'react-router-dom'
-import CustomerCRUD from'./components/Customer/CustomerCRUD'
+
 import LandingPage from './components/main/LandingPage';
 import Content from './components/main/Content';
 
@@ -13,7 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/"  component={LandingPage}/>
           <Route exact path="/content/:id"  component={Content}/>
-          <Route exact path="/board"  component={CustomerCRUD}/>
+
         </Switch>
       </Router>
    
